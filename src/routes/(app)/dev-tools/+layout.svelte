cdbbdd2688cbe46162af5b6489097f21dbbf2e7c
madmti <supermatrix9@gmail.com>
2026-04-10 16:07:31 -0400

    feat: base layout + .env example

---
+++ src/routes/(app)/dev-tools/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { PUBLIC_SHOW_DEV_TOOLS } from '$env/static/public';
+    const showDevTools = PUBLIC_SHOW_DEV_TOOLS === 'true';
+	let { children } = $props();
+</script>
+
+{#if showDevTools}
+	{@render children()}
+{/if}
---
+++ src/routes/(app)/dev-tools/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+</script>
+
+<div in:fly={{ y: 10, duration: 300, delay: 100 }} class="flex flex-col gap-6">
+    <h1 class="text-2xl font-bold">Dev Tools</h1>
+
+</div>
