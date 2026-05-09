2a2a4f1d180d2cb7bd987269983c129bcf886171
madmti <supermatrix9@gmail.com>
2026-05-09 00:22:02 -0400

    feat: Resumen on dashboard

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -49,6 +49,7 @@
 				<div class="flex flex-col gap-6 w-full min-w-0">
 					<div class="w-full min-w-0"><AcademicHealth /></div>
 					<div class="w-full min-w-0"><UpcomingEvents /></div>
+					<div class="w-full min-w-0"><Resumen /></div>
 				</div>
 			</div>
 		</div>
