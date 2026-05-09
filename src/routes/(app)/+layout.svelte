45996145d949113f48cc8a43746edf1aaed43533
madmti <supermatrix9@gmail.com>
2026-05-09 00:19:36 -0400

    fix: use trailingslash in navitems

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -50,9 +50,9 @@
 
 	<div class="bg-base-100 rounded-2xl hover:scale-105 transition-all duration-200">
 		<a
-			href={resolve('/horarios')}
+			href={resolve('/horarios/')}
 			class="group overflow-hidden relative rounded-2xl sm:px-6 py-4 border transition-all block
-            {isActive('/horarios')
+            {isActive('/horarios/')
 				? 'ring-2 ring-schedule-100 bg-schedule-400 border-schedule-300 text-schedule-100'
 				: 'border-base-400 text-content hover:border-schedule-300 hover:bg-schedule-400/50'}"
 		>
@@ -60,7 +60,7 @@
 				<CalendarCheck class="w-8 h-8 max-sm:w-6 max-sm:h-6 rounded-md sm:p-1" />
 				<span class="text-lg font-semibold max-sm:hidden truncate">Horarios</span>
 			</div>
-			{#if isActive('/horarios')}<div
+			{#if isActive('/horarios/')}<div
 					class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 h-2 w-8 bg-schedule-100 rounded-full"
 				></div>{/if}
 		</a>
@@ -68,9 +68,9 @@
 
 	<div class="bg-base-100 rounded-2xl hover:scale-105 transition-all duration-200">
 		<a
-			href={resolve('/notas')}
+			href={resolve('/notas/')}
 			class="group overflow-hidden relative rounded-2xl sm:px-6 py-4 border transition-all block
-            {isActive('/notas')
+            {isActive('/notas/')
 				? 'ring-2 ring-grades-100 bg-grades-400 border-grades-300 text-grades-100'
 				: 'border-base-400 text-content hover:border-grades-300 hover:bg-grades-400/50'}"
 		>
@@ -78,7 +78,7 @@
 				<TrendingUp class="w-8 h-8 max-sm:w-6 max-sm:h-6 rounded-md sm:p-1" />
 				<span class="text-lg font-semibold max-sm:hidden truncate">Notas</span>
 			</div>
-			{#if isActive('/notas')}<div
+			{#if isActive('/notas/')}<div
 					class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 h-2 w-8 bg-grades-100 rounded-full"
 				></div>{/if}
 		</a>
@@ -86,9 +86,9 @@
 
 	<div class="bg-base-100 rounded-2xl hover:scale-105 transition-all duration-200">
 		<a
-			href={resolve('/calendario')}
+			href={resolve('/calendario/')}
 			class="group overflow-hidden relative rounded-2xl sm:px-6 py-4 border transition-all block
-            {isActive('/calendario')
+            {isActive('/calendario/')
 				? 'ring-2 ring-calendar-100 bg-calendar-400 border-calendar-300 text-calendar-100'
 				: 'border-base-400 text-content hover:border-calendar-300 hover:bg-calendar-400/50'}"
 		>
@@ -96,7 +96,7 @@
 				<CalendarDays class="w-8 h-8 max-sm:w-6 max-sm:h-6 rounded-md sm:p-1" />
 				<span class="text-lg font-semibold max-sm:hidden truncate">Calendario</span>
 			</div>
-			{#if isActive('/calendario')}<div
+			{#if isActive('/calendario/')}<div
 					class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 h-2 w-8 bg-calendar-100 rounded-full"
 				></div>{/if}
 		</a>
@@ -104,9 +104,9 @@
 
 	<div class="bg-base-100 rounded-2xl hover:scale-105 transition-all duration-200">
 		<a
-			href={resolve('/ramos')}
+			href={resolve('/ramos/')}
 			class="group overflow-hidden relative rounded-2xl sm:px-6 py-4 border transition-all block
-            {isActive('/ramos')
+            {isActive('/ramos/')
 				? 'ring-2 ring-classes-100 bg-classes-400 border-classes-300 text-classes-100'
 				: 'border-base-400 text-content hover:border-classes-300 hover:bg-classes-400/50'}"
 		>
@@ -114,7 +114,7 @@
 				<BookMarked class="w-8 h-8 max-sm:w-6 max-sm:h-6 rounded-md sm:p-1" />
 				<span class="text-lg font-semibold max-sm:hidden truncate">Ramos</span>
 			</div>
-			{#if isActive('/ramos')}<div
+			{#if isActive('/ramos/')}<div
 					class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 h-2 w-8 bg-classes-100 rounded-full"
 				></div>{/if}
 		</a>
@@ -122,9 +122,9 @@
 
 	<div class="bg-base-100 rounded-2xl hover:scale-105 transition-all duration-200">
 		<a
-			href={resolve('/configuracion')}
+			href={resolve('/configuracion/')}
 			class="group overflow-hidden relative rounded-2xl sm:px-6 py-4 border transition-all block
-            {isActive('/configuracion')
+            {isActive('/configuracion/')
 				? 'ring-2 ring-config-100 bg-config-400 border-config-300 text-config-100'
 				: 'border-base-400 text-content hover:border-config-300 hover:bg-config-400/50'}"
 		>
@@ -132,7 +132,7 @@
 				<Bolt class="w-8 h-8 max-sm:w-6 max-sm:h-6 rounded-md sm:p-1" />
 				<span class="text-lg font-semibold max-sm:hidden truncate">Config</span>
 			</div>
-			{#if isActive('/configuracion')}
+			{#if isActive('/configuracion/')}
 				<div
 					class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 h-2 w-8 bg-config-100 rounded-full"
 				></div>
@@ -143,9 +143,9 @@
 	{#if showDevTools}
 		<div class="bg-base-100 rounded-2xl hover:scale-105 transition-all duration-200">
 			<a
-				href={resolve('/dev-tools')}
+				href={resolve('/dev-tools/')}
 				class="group overflow-hidden relative rounded-2xl sm:px-6 py-4 border transition-all block
-            {isActive('/dev-tools')
+            {isActive('/dev-tools/')
 					? 'ring-2 ring-config-100 bg-config-400 border-config-300 text-config-100'
 					: 'border-base-400 text-content hover:border-config-300 hover:bg-config-400/50'}"
 			>
@@ -153,7 +153,7 @@
 					<Bone class="w-8 h-8 max-sm:w-6 max-sm:h-6 rounded-md sm:p-1" />
 					<span class="text-lg font-semibold max-sm:hidden truncate">Dev Tools</span>
 				</div>
-				{#if isActive('/dev-tools')}
+				{#if isActive('/dev-tools/')}
 					<div
 						class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 h-2 w-8 bg-config-100 rounded-full"
 					></div>
