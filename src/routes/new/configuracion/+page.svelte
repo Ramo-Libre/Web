43d8eab07c6fcb62dee96de0151fc0bc7421daaf
madmti <supermatrix9@gmail.com>
2026-06-28 23:31:03 -0400

    feat: basic config

--- src/routes/new/configuracion/+page.svelte
+++ src/routes/new/configuracion/+page.svelte
@@ -1,3 +1,15 @@
 <script lang="ts">
-
+	import Apariencia from './_components/Apariencia.svelte';
+	import About from './_components/About.svelte';
 </script>
+
+<div class="w-full">
+	<div class="columns-1 lg:columns-2 gap-6 space-y-6">
+		<div id="apariencia" class="break-inside-avoid">
+			<Apariencia />
+		</div>
+		<div id="about" class="break-inside-avoid">
+			<About />
+		</div>
+	</div>
+</div>
